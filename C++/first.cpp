3c16134d34dd997712265003168ca5f79131eb78
SPOOK_EXE <91944990+SPOOKEXE@users.noreply.github.com>
2022-07-14 17:56:09 +0930

    important note

--- C++/first.cpp
+++ C++/first.cpp
@@ -1,9 +1,11 @@
 
 #include <iostream>
 
-using namespace std;
-
 int main() {
+	// don't use in global scope, bad practice and can mix libraries up
+	using namespace std;
+
 	cout << "Epic!";
+
 	return 1;
 }
